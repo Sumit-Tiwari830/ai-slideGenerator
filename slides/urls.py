afc5ad9cd5e478f5451737cedca6667247830e62
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-05-20 09:06:44 +0000

    tailwind setup done

--- slides/urls.py
+++ slides/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.slide_builder, name='slide_builder'),
-    path('api/generate_slides/', views.generate_slides, name='generate_slides'),
-    path('api/share/', views.share_slides, name='share_slides'),
-    path('view/<str:code>/', views.view_shared, name='view_shared'),
+   
 ]
--- slides/views.py
+++ slides/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def slide_builder(request):return render(request,'index.html');
